4544d1bd186673d9d44b71c670df86c91cbce66e
Jarek Kapica <jaroslaw.kapica@gmail.com>
2021-11-01 17:44:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import csv
+from time import sleep
+from random import choice, shuffle
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+cols = ["infinitive", "past tense", "past participle", "pl"]
+
+
+def shuffled(collection):
+    col = collection.copy()
+    shuffle(col)
+    return col
+
+
+def main(argv):
+    verbs = dict()
+    
+    with open(os.path.join(HERE, 'verbs.csv')) as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=cols)
+        for i, row in enumerate(reader):
+            verbs[i] = row
+
+    while verbs:
+        idx = choice(list(verbs.keys()))
+        verb = verbs[idx]
+        print(f'\n{idx}>>> {verb["pl"]}?')    
+        
+        answers = []
+        for c in shuffled(cols[:3]):
+            print(f"{c}: ", end='')
+            answer = input()
+            if answer == 'q':
+                break
+            if answer == verb[c]:
+                print("OK")
+                answers.append(True)
+            else:
+                print(f"wrong, correct is: {verb[c]}")
+                answers.append(False)
+
+        if all(answers):
+            print(f"{idx}: {', '.join(verb)} -> All OK")
+            verbs.pop(idx)
+        sleep(0.5)
+
+    print('BRAVO!')
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
